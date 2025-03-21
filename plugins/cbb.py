30f60820eb503dd845686dfb4c962385a8ca42c3
Project999x <105520683+Project999x@users.noreply.github.com>
2025-03-21 14:13:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -43,7 +43,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "premium":
         await query.message.edit_text(
-            text=f"Premium Benefits & Perks\nDirect Channel Links, No Ad Links\nSpecial Access In Events\n\n<blockquote>Pricing Rates\n1 Month - INR 100\n3 Months - INR 250\n6 Months - INR 400\n12 Month - Contact <a href=https://t.me/Kira_Yagamai>Owner</a></blockquote>\n\n<blockquote>Want To Buy?\n<a href=https://t.me/Kira_Yagamai>Owner</a></blockquote>\n\nWe Have Limited Seats For Premium Users",
+            text=f"@Hentai_Cinema Premium Benefits & Perks\nDirect Channel Links, No Ad Links\nSpecial Access In Events\n\n1. 𝗖𝗼𝗿𝗻_𝗖𝗶𝗻𝗲𝗺𝗮\n2. 𝗰𝗼𝘀𝗽𝗹𝗮𝘆_𝗖𝗶𝗻𝗲𝗺𝗮\n3. 𝗛𝗲𝗻𝘁𝗮𝗶_𝗖𝗶𝗻𝗲𝗺𝗮\n4. 𝗜𝗻𝗱𝗶𝗮𝗻_𝗖𝗶𝗻𝗲𝗺𝗮\n5. 𝗢𝗻𝗹𝘆_𝗳𝗮𝗻𝘀_𝗰𝗶𝗻𝗲𝗺𝗮\n6. 𝗽𝗮𝗿𝗼𝗱𝘆_𝗰𝗶𝗻𝗲𝗺𝗮\n7. Japanese_Cinema\n8. 𝗛𝗲𝗻𝘁𝗮𝗶 𝗜𝗻 𝗛𝗶𝗻𝗱𝗶 \n9. 𝗙𝗮𝗺𝗶𝗹𝘆 𝗙𝗮𝗻𝘁𝗮𝘀𝘆 \n<blockquote>Pricing Rates\n1 Month - INR 100\n3 Months - INR 250\n6 Months - INR 400\n12 Month - Contact <a href=https://t.me/Kira_Yagamai>Kira Yagamai</a></blockquote>\n\n<blockquote>Want To Buy?\n<a href=https://t.me/Kira_Yagamai>Kira Yagamai</a></blockquote>\n\nWe Have Limited Seats For Premium Users",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
